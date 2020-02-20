709709d0585d49a0bceaf10b71087c7cbb4b291a
squirrel84 <lukesgspires@gmail.com>
2020-02-20 00:34:07 +0000

    Adding BayPrediction with RowPrediction Added functionality to highlight predicted box for selected box

--- ContainerPlanPredictionApp/Services/ShippingRoutePredictionService.cs
+++ ContainerPlanPredictionApp/Services/ShippingRoutePredictionService.cs
@@ -1,13 +1,19 @@
 ﻿using ContainerPlanPrediction.Models;
 using ContainerPlanPredictionML.Model;
+using System;
 
-namespace ContainerPlanPrediction.Data
+namespace ContainerPlanPrediction.Services
 {
     public class ShippingRoutePredictionService
     {
+        ConsumeModel ConsumeModel = null;
         public ShippingRoutePredictionService()
         {
-            
+            string currentDirectory = System.Environment.CurrentDirectory;
+            var modelDirectory = currentDirectory.Substring(0, currentDirectory.LastIndexOf("\\")) + "\\ContainerPlanPredictionML.Model\\";
+
+            ConsumeModel.Initialise(modelDirectory);
+            ConsumeModel = ConsumeModel.CurrentModel;
         }
 
         public string PredictRow(ContainerDestination containerDestination)
@@ -22,7 +28,25 @@ namespace ContainerPlanPrediction.Data
             };
 
             // Load model and predict output of sample data
-            ModelOutput result = ConsumeModel.Predict(input);
+            ModelOutput result = ConsumeModel.PredictRow(input);
+
+            return result.Prediction.ToString();
+        }
+
+        public string PredictBay(ContainerDestination containerDestination, int row)
+        {
+            // Add input data
+            var input = new ModelInput
+            {
+                Row = row,
+                ContainerContentType = containerDestination.Container.ContentType.ToString(),
+                ContainerSize = containerDestination.Container.Size.ToString(),
+                ContainerType = containerDestination.Container.ContainerType.ToString(),
+                RouteNumber = containerDestination.Destination.RegionNumber
+            };
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.PredictBay(input);
 
             return result.Prediction.ToString();
         }
